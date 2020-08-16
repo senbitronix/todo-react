ed21444c587174aee39164d545677e1e2ddf571b
Vasya <senbitronix@gmail.com>
2020-08-16 10:50:29 +0300

    react todo1

---
+++ precache-manifest.0f0fe7ab4afa7d1620b834de7782f708.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc05020426a287257ba779b862b5777c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21c2bbf03a0efc7e4db1",
+    "url": "/static/css/main.299c5a0c.chunk.css"
+  },
+  {
+    "revision": "ecf9c7ba341e5b4bd5ed",
+    "url": "/static/js/2.a3706892.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a3706892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21c2bbf03a0efc7e4db1",
+    "url": "/static/js/main.a1d442c2.chunk.js"
+  },
+  {
+    "revision": "310c168f5b4a8bb90b3d",
+    "url": "/static/js/runtime-main.eefa256f.js"
+  }
+]);
